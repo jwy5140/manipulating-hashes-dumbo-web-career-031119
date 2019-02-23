1baa450fbba850c29833e6cab9098cc9ae726803
Jeffrey Yu <jwy5140@gmail.com>
2019-02-23 09:32:31 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,9 +12,14 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-  binding.pry
   #your code here
-  contacts.each {|x,y|puts x}
+  contacts.each {|x,y|
+  if x == "Freddy Mercury"
+    x.each {|x,y|
+    if x == :favorite_icecream_flavors && x.include?("strawberry")
+      x.d
+      
+  }
     
 
   #remember to return your newly altered contacts hash!
