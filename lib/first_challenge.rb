f5e7584f3f62c06e787a3ec940d9dd52d4c6c7e1
Jeffrey Yu <jwy5140@gmail.com>
2019-02-23 09:47:04 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,9 +12,19 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-
   #your code here
-
+  contacts.each {|x,y|
+  if x == "Freddy Mercury"
+    y.each {|x,y|
+    if x == :favorite_icecream_flavors && y.include?("strawberry")
+      y.delete("strawberry")
+    else
+      puts "Hey! I thought strawberry was one of your faves!"
+    end
+    }
+  end
+  }
+    
 
   #remember to return your newly altered contacts hash!
   contacts
