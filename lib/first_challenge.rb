7c9bb0164e556b27a85085d5fd8b29156fc4fb19
Jeffrey Yu <jwy5140@gmail.com>
2019-02-23 09:38:55 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,13 +15,15 @@ def first_challenge
   #your code here
   contacts.each {|x,y|
   if x == "Freddy Mercury"
-    x.each {|x,y|
+    contacts[x].each {|x,y|
     if x == :favorite_icecream_flavors && x.include?("strawberry")
       x.delete("strawberry")
     else
       puts "I thought strawberry was one of your faves, Freddy!"
     end
     }
+  else
+    puts "Who in the world is Freddy Mercury?"
   end
   }
     
